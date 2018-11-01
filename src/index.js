6f248f5d8b79ab32c717595d5c765fcd0e6f2d72
Lauren Chavez <laurenchavez@Laurens-MacBook-Pro.local>
2018-11-01 13:06:28 -0700

    basic components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(<App />, document.getElementById('root'));
