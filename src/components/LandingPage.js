f2d62c9190e6134b61ef078a7f169bb45a4720ab
tyler yazzie <apple@Tyler.local>
2018-11-02 10:56:26 -0700

    1234

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,16 +1,4 @@
 import React, { Component } from 'react';  
-// import {
-//     Collapse,
-//     Navbar,
-//     NavbarToggler,
-//     NavbarBrand,
-//     Nav,
-//     NavItem,
-//     NavLink,
-//     UncontrolledDropdown,
-//     DropdownToggle,
-//     DropdownMenu,
-//     DropdownItem } from 'reactstrap';
 
 
 
